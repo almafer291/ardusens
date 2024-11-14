06e057acba514fa9c10e3ddd3e4402e654aceaa3
almafer291 <almafer291@gmail.com>
2024-11-14 12:54:48 +0100

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
   try {
-    const response = await fetch('http://https://e41d-2a02-9130-88a7-7ca8-2090-d949-3d01-8d47.ngrok-free.app//sensores');  // Cambia TU_URL_DE_NGROK por la URL de ngrok
+    const response = await fetch('http://https://e41d-2a02-9130-88a7-7ca8-2090-d949-3d01-8d47.ngrok-free.app/sensores');  // Cambia TU_URL_DE_NGROK por la URL de ngrok
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
