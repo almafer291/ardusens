dc9aff4a6a7ac71b9a4ef8f170448d55bb11f99a
almafer291 <almafer291@gmail.com>
2024-12-16 18:40:37 +0100

    Update and rename firebase.js to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,22 @@
+// Importar las funciones necesarias de Firebase
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
+// Configuración de tu Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBJT5ckT_Os1eTxPvVn9kjFi3pXXEUeIe8",
   authDomain: "ardusens.firebaseapp.com",
-  databaseURL: "https://ardusens-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://ardusens-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "ardusens",
   storageBucket: "ardusens.firebasestorage.app",
   messagingSenderId: "932230234372",
   appId: "1:932230234372:web:f68c12d2913155e30a9051",
+  measurementId: "G-JBXRDGDTY7"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const database = getDatabase(app);
 
-export { db, ref, onValue };
+// Exportar la base de datos para usarla en otros archivos
+export { database, ref, onValue };
